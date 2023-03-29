bd8513090542f4b9854f577f4bf66eecb70bb12c
Ben Sivyer <bensivyer@bens-mbp-2.home>
2023-03-29 08:22:58 +0100

    5.Get/api/articles : complete endpoint and testing

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/topics.controllers");
-const { getArticleById } = require("./controllers/articles.controllers");
+const {
+  getArticles,
+  getArticleById,
+} = require("./controllers/articles.controllers");
 
 app.get("/api/topics", getTopics);
+
+app.get("/api/articles", getArticles);
 
 app.get("/api/articles/:article_id", getArticleById);
 
@@ -29,5 +34,3 @@ app.use("/*", (request, response) => {
 });
 
 module.exports = app;
-
-// .NEXT
--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -1,9 +1,18 @@
 const { response } = require("../app");
-const { fetchArticle } = require("../models/articles.models.js");
+const {
+  fetchArticleById,
+  fetchArticles,
+} = require("../models/articles.models.js");
+
+exports.getArticles = (request, response) => {
+  fetchArticles().then((articles) => {
+    response.status(200).send({ articles: articles });
+  });
+};
 
 exports.getArticleById = (request, response, next) => {
   const { article_id } = request.params;
-  fetchArticle(article_id)
+  fetchArticleById(article_id)
     .then((article) => {
       response.status(200).send({ article });
     })
